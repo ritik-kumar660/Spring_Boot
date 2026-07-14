229400307e1a80959aad3a74fb1bc627b3b0772a
Ritik Kumar <ritikkumarharhar660@gmail.com>
2026-07-14 12:03:27 +0530

    first commit

--- src/main/java/com/mohit/demo/Portfolio/MyPortfolio.java
+++ src/main/java/com/mohit/demo/Portfolio/MyPortfolio.java
@@ -7,14 +7,52 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyPortfolio {
 
     @GetMapping("/mySelf")
+
     public String Myself() {
     return """
             <h1>Myself</h1>
-            <p> simple line about yourself </p>
+            <h3> Hii My Name is Ritik Kumar I am A Final year B.Tech. CSE Student at LPU </h3>
             <ul>
-            <li>gitHub Link : link</li>
-            <li>Leetcode Link : link link</li>
+            <li><a target=_blank href="https://github.com/ritik-kumar660">gitHub</li>
+            <li><a href="https://github.com/ritik-kumar660">LeetCode</li>
             </ul>
             """;
     }
+    @GetMapping("/skills")
+    public String Skills(){
+        return """
+                <h1> Tech Stack</h1>
+                <ul>
+                <li>Python</li>
+                <li>Java</li>
+                <li>CPP</li>
+                <li>React</li>
+                <li>Node</li>
+                </ul>
+                
+                """;
+    }
+    @GetMapping("/education")
+    public String getEducation(){
+        return """
+                <h1>Education Page</h1>
+                """;
+    }
+    @GetMapping("/projects")
+    public String getProjects(){
+        return """
+                <h1>Projects Page</h1>
+                """;
+    }
+    @GetMapping("/contact")
+    public String getContact(){
+        return """
+                <h1>Contact Page</h1>
+                """;
+    }
+
+
+
+
+
 }
