0412a2ede692c25ca9a483c8ed45e415472dfeab
Ritik Kumar <ritikkumarharhar660@gmail.com>
2026-07-15 11:42:35 +0530

    Spring data JPA

--- src/main/java/com/mohit/demo/StudentServer/Student.java
+++ src/main/java/com/mohit/demo/StudentServer/Student.java
@@ -1,7 +1,12 @@
 package com.mohit.demo.StudentServer;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student
 {
+    @Id
     int id;
     String name;
     int age;
--- src/main/java/com/mohit/demo/StudentServer/StudentRepository.java
+++ src/main/java/com/mohit/demo/StudentServer/StudentRepository.java
@@ -1,14 +1,7 @@
 package com.mohit.demo.StudentServer;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-@Repository
-public class StudentRepository {
-    public Student save(Student student){
-        System.out.println("Student data saved ");
-        return student;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-    }
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 }
